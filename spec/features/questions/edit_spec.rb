8ffaa2ec83ab6e26fabdcb494e41bd8f0bc21786
Vitaliy Dubrovskiy <dubrovskiyvitaliy@yandex.ru>
2023-05-24 20:13:06 +0500

    feat: author can add files to questions while editing

--- spec/features/questions/edit_spec.rb
+++ spec/features/questions/edit_spec.rb
@@ -40,6 +40,21 @@ feature 'User can edit his question' do
 
       expect(page).to have_no_link('Edit')
     end
+
+    scenario "adds files while editing" do
+      click_on 'Edit'
+
+      within '.question' do
+        fill_in 'Edit question', with: 'edit body'
+
+        attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+        click_on 'Save'
+
+        expect(page).to have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
+      end
+
+    end
   end
 
   scenario 'Unauthenticated user tries to edit question', js: true do
