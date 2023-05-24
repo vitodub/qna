6d0bb636c98275dc92c3e8a38ce04a0091bf2054
Vitaliy Dubrovskiy <dubrovskiyvitaliy@yandex.ru>
2023-05-24 20:40:56 +0500

    feat: author can add files to answers while editing

--- spec/features/answers/edit_spec.rb
+++ spec/features/answers/edit_spec.rb
@@ -48,5 +48,18 @@ feature 'User can edit his answer', %q{
     scenario "tries to edit other user's question", js: true do
       expect(page).to have_link('Edit', count: 1)      
     end
+
+    scenario "adds files while editing", js: true do
+      within '.answers' do
+        click_on 'Edit'
+        fill_in 'Edit answer', with: 'edited answer'
+
+        attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+        click_on 'Save'
+            
+        expect(page).to have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
+      end
+    end
   end
 end
