2ed1b621fc009596e3c11e4527a0ab8d5b488cb3
Vitaliy Dubrovskiy <dubrovskiyvitaliy@yandex.ru>
2023-05-30 23:18:14 +0500

    feat: author can add links to questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -12,6 +12,7 @@ class QuestionsController < ApplicationController
 
   def new
     @question = Question.new
+    @question.links.new # .build
   end
 
   def edit
@@ -48,6 +49,6 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    params.require(:question).permit(:title, :body, files: [])
+    params.require(:question).permit(:title, :body, files: [], links_attributes: [:name, :url])
   end
 end
